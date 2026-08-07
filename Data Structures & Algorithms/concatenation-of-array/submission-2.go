1524f927529bdc2aade8bf76e7d111a91375f6ef
Kevin Kolcheck <kevin.kolcheck@bonterratech.com>
2026-08-07 09:11:16 -0400

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.go
@@ -0,0 +1,10 @@
+func getConcatenation(nums []int) []int {
+    // time: O(n), space:O(1)
+    n := len(nums);
+    ans := make([]int, n*2);
+    for i := 0; i < n; i++ {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+    return ans;
+}
